19a59ac6decaf470b80d1f1f04b63e1ca7373a6b
RAHUL-1881 <gettheshot1881@gmail.com>
2023-09-14 21:58:07 +0530

    nothing much

--- 100_days_grind/modn_counter/modn_count.sv
+++ 100_days_grind/modn_counter/modn_count.sv
@@ -5,12 +5,12 @@ module modn_counter (clk,out);
 
 input clk;
 output [2:0]out;
-wire w1,w2,w3,w4,wz;
+wire w1,w2,w3,wz;
 
-assign wz = w1 & w2 & ~w3;
-pre_clr gh (.j(1'b1),.k(1'b1),.pre(wz),.clr(1'b1),.clk(clk),.q(w1));
-pre_clr hj (.j(1'b1),.k(1'b1),.pre(wz),.clr(1'b1),.clk(w1),.q(w2));
-pre_clr op (.j(1'b1),.k(1'b1),.pre(wz),.clr(1'b1),.clk(w2),.q(w3));
+assign wz = w1 & ~w2 & ~w3;
+pre_clr gh (.j(1'b1),.k(1'b1),.pre(1'b1),.clr(wz),.clk(clk),.q(w1));
+pre_clr hj (.j(1'b1),.k(1'b1),.pre(1'b1),.clr(wz),.clk(w1),.q(w2));
+pre_clr op (.j(1'b1),.k(1'b1),.pre(1'b1),.clr(wz),.clk(w2),.q(w3));
 assign out[0] = w1;
 assign out[1] = w2;
 assign out[2] = w3;
